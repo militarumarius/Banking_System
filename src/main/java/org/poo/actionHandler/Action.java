f0b462ed14c21bc8e372ac8aa8b4356132963ed2
ionut.militaru0801 <ionut.militaru0801@stud.acs.upb.ro>
2024-12-11 21:49:50 +0200

    implementare delete account

--- src/main/java/org/poo/actionHandler/Action.java
+++ src/main/java/org/poo/actionHandler/Action.java
@@ -2,15 +2,16 @@ package org.poo.actionHandler;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.poo.bank.BankDatabase;
 import org.poo.bank.User;
 import org.poo.bank.accounts.Account;
 import org.poo.bank.accounts.FactoryAccount;
 import org.poo.bank.cards.DebitCard;
+import org.poo.bank.cards.OneTImeUseCard;
 import org.poo.fileio.CommandInput;
 import org.poo.fileio.ObjectInput;
-import org.poo.fileio.UserInput;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -56,11 +57,51 @@ public class Action {
                                 if(account.getIBAN().equals(commandInput.getAccount()))
                                     account.setBalance(account.getBalance() + commandInput.getAmount());
                             }
+                    }
+
+                }
+                case "createOneTimeCard" ->{
+                    for(User user : bank.getUsers()) {
+                        if(user.getEmail().equals(commandInput.getEmail())) {
+                            for(Account account : user.getAccounts()){
+                                if(account.getIBAN().equals(commandInput.getAccount()))
+                                    account.getCards().add(new OneTImeUseCard(generateCardNumber(), 0));
+                            }
                         }
 
                     }
                 }
+                case "deleteCard" ->{
+                    for(User user : bank.getUsers()) {
+                        if(user.getEmail().equals(commandInput.getEmail())) {
+                            for(Account account : user.getAccounts()){
+                                if(account.getType().equals("classic"))
+                                    account.getCards().removeIf(card -> card.getCardNumber().equals(commandInput.getCardNumber()));
+
+                            }
+                        }
+
+                    }
+                }
+                case "deleteAccount" -> {
+                    boolean check = false;
+                    ObjectNode node = JsonNodeFactory.instance.objectNode();
+                    objectNode.put("command", "deleteAccount");
+                    for(User user : bank.getUsers()) {
+                        if(user.getEmail().equals(commandInput.getEmail()))
+                            check = user.getAccounts().removeIf(account ->
+                                    account.getIBAN().equals(commandInput.getAccount()) && account.getBalance() == 0);
+                    }
+                    if(check) {
+                        node.put("success", "Account deleted");
+                        node.put("timestamp", commandInput.getTimestamp());
+                    }
+                    objectNode.putPOJO("output", node);
+                    objectNode.put("timestamp", commandInput.getTimestamp());
+                    output.addPOJO(objectNode);
+                }
             }
         }
     }
+}
 
